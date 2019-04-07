11ef0637013890810a0bd159ce503f842187226f
yuting49 <ytcstone@gmail.com>
2019-04-07 14:57:20 +0800

    Updates

---
+++ precache-manifest.b9c68ec161a76a4d5447db278fbd9f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51f373716cdb9386144f",
+    "url": "/React-gomoku/static/css/main.d3e40ab4.chunk.css"
+  },
+  {
+    "revision": "51f373716cdb9386144f",
+    "url": "/React-gomoku/static/js/main.96193b82.chunk.js"
+  },
+  {
+    "revision": "ac805c80e94b78030156",
+    "url": "/React-gomoku/static/js/runtime~main.93ea0d7a.js"
+  },
+  {
+    "revision": "a1070a7a8f26b8991c52",
+    "url": "/React-gomoku/static/css/2.719286ae.chunk.css"
+  },
+  {
+    "revision": "a1070a7a8f26b8991c52",
+    "url": "/React-gomoku/static/js/2.c0c20dd6.chunk.js"
+  },
+  {
+    "revision": "1f7370f7400a6e37d2c791f7bc9254bf",
+    "url": "/React-gomoku/index.html"
+  }
+];
